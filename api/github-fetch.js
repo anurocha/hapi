a625d0cc857f65b85f37973df096e4ecba47343d
anurocha <anurocha@gmail.com>
2021-08-04 11:07:18 +0700

    (chor) add private field

--- api/github-fetch.js
+++ api/github-fetch.js
@@ -1,9 +1,9 @@
 const got = require('got');
 class GitHubFetch {
     #GITHUB_URL = 'https://api.github.com/search/code?q';
-    _fetcher = got;
+    #_fetcher = got;
     init (fetcher) {
-        this._fetcher = fetcher;
+        this.#_fetcher = fetcher;
     }
     async fetch ( query, pageNumber, resultPerPage ) {
         if ( query === null || query === undefined ) {
@@ -12,7 +12,7 @@ class GitHubFetch {
         pageNumber === null || pageNumber === undefined ? pageNumber = 1: null ;
         resultPerPage = 10;
         const url = `${this.#GITHUB_URL}=${query}+user:mozilla&page=${pageNumber}&per_page=${resultPerPage}`;
-        const {body} = await this._fetcher.get( url, {
+        const {body} = await this.#_fetcher.get( url, {
             responseType: 'json'
         });
 
