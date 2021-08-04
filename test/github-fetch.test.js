b5751961bb00d7d8f576a3fd5a06c66715ff57d3
anurocha <anurocha@gmail.com>
2021-08-04 10:28:48 +0700

    (chor) improve tests

--- test/github-fetch.test.js
+++ test/github-fetch.test.js
@@ -1,16 +1,30 @@
 console.log('--- Test github-fetch ---');
 
+const GitH = require("../api/github-fetch");
+
+const ASSESRT_RESULT = {
+    PASSED : 'Passed',
+    FAILED : 'Failed'
+};
+
 class MockFetcher {
+    _mockResponse;
+    constructor(mockResponse) {
+        this._mockResponse = mockResponse;
+    }
     get = async () => {
-        return JSON.parse('{"body":[]}');
+        return JSON.parse(this._mockResponse);
     }
 }
 
-(async () => {
-    const GitH = require("../api/github-fetch");
+(async (testName) => {
     const gh = new GitH();
-    gh.init(new MockFetcher());
+    gh.init(new MockFetcher('{"body":{"total_count":0,"incomplete_results":false,"items":[{"name":"mozcountdown.js","path":"media/js/libs/mozcountdown.js","sha":"8dfa9213e5df648b9d022c02c5e79ef3d4cdb7b0","url":"https://api.github.com","git_url":"https://api.github.com","html_url":"https://github.com","repository":{}}]}}'));
+    const expectedOutput = '{"total_count":0,"incomplete_results":false,"items":[{"name":"mozcountdown.js","path":"media/js/libs/mozcountdown.js","sha":"8dfa9213e5df648b9d022c02c5e79ef3d4cdb7b0","url":"https://api.github.com","git_url":"https://api.github.com","html_url":"https://github.com","repository":{}}]}';
     
-    const gresult = await gh.fetch('node', 3, 10);
-    console.log(JSON.stringify(gresult));
-})();
+    const result = await gh.fetch('node', 3, 10);
+
+    let assertResult;
+    JSON.stringify(result) === expectedOutput ? assertResult = ASSESRT_RESULT.PASSED : assertResult = ASSESRT_RESULT.FAILED;
+    console.log(`${testName} : ${assertResult}`);
+})('Valid Case');
--- test/restructure.test.js
+++ test/restructure.test.js
@@ -3,9 +3,32 @@ console.log('--- Test restructure ---');
 const RestructApi = require("../api/restructure");
 const api = new RestructApi();
 
-const testinput = '{"0":[{"id":10,"title":"House","level":0,"children":[],"parent_id":null}],"1":[{"id":12,"title":"Red Roof","level":1,"children":[],"parent_id":10},{"id":18,"title":"Blue Roof","level":1,"children":[],"parent_id":10},{"id":13,"title":"Wall","level":1,"children":[],"parent_id":10}],"2":[{"id":17,"title":"Blue Window","level":2,"children":[],"parent_id":12},{"id":16,"title":"Door","level":2,"children":[],"parent_id":13},{"id":15,"title":"Red Window","level":2,"children":[],"parent_id":12}]}';
+const ASSESRT_RESULT = {
+    PASSED : 'Passed',
+    FAILED : 'Failed'
+};
 
-const result = api.transform(testinput);
+// --- valid case ---
+((testName) => {
+    const testinput = '{"0":[{"id":10,"title":"House","level":0,"children":[],"parent_id":null}],"1":[{"id":12,"title":"Red Roof","level":1,"children":[],"parent_id":10},{"id":18,"title":"Blue Roof","level":1,"children":[],"parent_id":10},{"id":13,"title":"Wall","level":1,"children":[],"parent_id":10}],"2":[{"id":17,"title":"Blue Window","level":2,"children":[],"parent_id":12},{"id":16,"title":"Door","level":2,"children":[],"parent_id":13},{"id":15,"title":"Red Window","level":2,"children":[],"parent_id":12}]}';
+    const expectedOutput = '[{"id":10,"title":"House","level":0,"children":[{"id":12,"title":"Red Roof","level":1,"children":[{"id":17,"title":"Blue Window","level":2,"children":[],"parent_id":12},{"id":15,"title":"Red Window","level":2,"children":[],"parent_id":12}],"parent_id":10},{"id":18,"title":"Blue Roof","level":1,"children":[],"parent_id":10},{"id":13,"title":"Wall","level":1,"children":[{"id":16,"title":"Door","level":2,"children":[],"parent_id":13}],"parent_id":10}],"parent_id":null}]';
+    const result = api.transform(testinput);
 
-console.log(`input = ${testinput}`);
-console.log(`output = ${JSON.stringify(result)}`);
+    // console.log(`input = ${testinput}`);
+    // console.log(`output = ${JSON.stringify(result)}`);
+    let assertResult;
+    JSON.stringify(result) === expectedOutput ? assertResult = ASSESRT_RESULT.PASSED : assertResult = ASSESRT_RESULT.FAILED;
+    console.log(`${testName} : ${assertResult}`);
+})('Valid Case');
+
+// --- invalid case ---
+((testName) => {
+    const testinput = '{xxxxx}';
+    const expectedOutput = '{"error":" :( "}';
+
+    const result = api.transform(testinput);
+
+    let assertResult;
+    JSON.stringify(result) === expectedOutput ? assertResult = ASSESRT_RESULT.PASSED : assertResult = ASSESRT_RESULT.FAILED;
+    console.log(`${testName} : ${assertResult}`);
+})('Invalid Case');
